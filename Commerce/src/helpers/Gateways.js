c62fa61fb9485641fff4d9ad433c66693995aa29
Nicolas Hernandez <nicoo.her@hotmail.com>
2018-10-20 15:14:57 -0300

    - Add support for some gateways

---
+++ Commerce/src/helpers/Gateways.js
@@ -0,0 +1,11 @@
+const Gateways = {
+  Deportes: 'Gateway A',
+  Electrodomesticos: 'Gateway B',
+  Comida: 'Gateway C',
+  Ropa: 'Gateway D',
+  Videojuegos: 'Gateway E',
+  Bazar: 'Gateway F',
+  Ferreteria: 'Gateway G',
+};
+
+export default Gateways;
