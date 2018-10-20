5b66d1248fcc589f05147e2a97633806f4feea3d
Nicolas Hernandez <nicoo.her@hotmail.com>
2018-10-20 13:09:52 -0300

    - Refactoring networking project.

---
+++ Network/src/helpers/ApiConstants.js
@@ -0,0 +1,5 @@
+const ApiConstants = {
+  PAY_YOU_NOW_API: 'http://localhost:8001',
+};
+
+export default ApiConstants;
---
+++ Network/src/networking/controllers/NetworkController.js
@@ -0,0 +1,9 @@
+import HttpService from '../HttpService';
+
+class NetworkController {
+  constructor() {
+    this.BASE_API = '/Network';
+  }
+}
+
+export default new NetworkController();
