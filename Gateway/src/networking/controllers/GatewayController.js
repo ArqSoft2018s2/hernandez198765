c8b16eea82e917b16e95bb79fed57834809fa27d
Nicolas Hernandez <nicoo.her@hotmail.com>
2018-10-20 22:45:32 -0300

    - Add validate credit cards, support for errors

--- Gateway/src/networking/controllers/GatewayController.js
+++ Gateway/src/networking/controllers/GatewayController.js
@@ -1,9 +1,40 @@
-import HttpService from '../HttpService';
+// import HttpService from '../HttpService';
+import CreditCards from '../../helpers/CreditCards';
 
 class GatewayController {
   constructor() {
     this.BASE_API = '/Gateway';
   }
+
+  getNetworkFromCreditCard = transaction => {
+    const {
+      card: { number },
+    } = transaction;
+    if (CreditCards.visaRegEx.test(number)) {
+      return 'Visa';
+    }
+    if (CreditCards.mastercardRegEx.test(number)) {
+      return 'MasterCard';
+    }
+    if (CreditCards.discovRegEx.test(number)) {
+      return 'Discover';
+    }
+    if (CreditCards.amexpRegEx.test(number)) {
+      return 'Amex';
+    }
+    throw new Error('Invalid Credit Card');
+  };
+
+  identifyNetwork = transaction => {
+    try {
+      return {
+        ...transaction,
+        network: this.getNetworkFromCreditCard(transaction),
+      };
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
 }
 
 export default new GatewayController();
