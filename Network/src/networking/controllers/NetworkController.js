65cec1e1b0c9040650dadf04bb971fdd52f3ffad
Nicolas Hernandez <nicoo.her@hotmail.com>
2018-10-20 23:31:39 -0300

    - Add dotenv for .env environment

--- Network/src/networking/controllers/NetworkController.js
+++ Network/src/networking/controllers/NetworkController.js
@@ -1,9 +1,30 @@
-import HttpService from '../HttpService';
+import dotenv from 'dotenv';
+import moment from 'moment';
+import DatabaseManager from '../../managers/DatabaseManager';
+// import HttpService from '../HttpService';
 
 class NetworkController {
   constructor() {
+    dotenv.config();
+    this.FRAUD_LIMIT = process.env.FRAUD_LIMIT;
     this.BASE_API = '/Network';
   }
+
+  fraudControl = transaction => {
+    const {
+      card: { number },
+    } = transaction;
+    const today = moment.unix();
+    const threeDaysAgo = moment()
+      .add(-3, 'days')
+      .unix();
+    DatabaseManager.getQuantityOfTransactionsBetweenHours(
+      number,
+      (status, collection) => {
+        console.log(collection);
+      },
+    );
+  };
 }
 
 export default new NetworkController();
