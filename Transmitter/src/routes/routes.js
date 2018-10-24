6e4945192c867e6c0ab4018ea8509d580f3b9dcb
Nicolas Hernandez <nicoo.her@hotmail.com>
2018-10-23 21:50:42 -0300

    - Add endpoint for add new Card.

--- Transmitter/src/routes/routes.js
+++ Transmitter/src/routes/routes.js
@@ -1,6 +1,18 @@
+import TransmitterController from '../networking/controllers/TransmitterController';
+
 const appRouter = app => {
   app.get('/test', (req, res) => {
     res.status(200).send('Communicate with Transmitter successful');
+  });
+
+  app.post('/Card', async (req, res) => {
+    try {
+      const newCard = req.body;
+      await TransmitterController.addNewCard(newCard);
+      res.status(200).send('Add new card succesfully');
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   });
 };
 
