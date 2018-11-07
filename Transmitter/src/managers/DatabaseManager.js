66938433e637beb42ddd9dd22634c117eaefd474
Nicolas Hernandez <nicolas@xmartlabs.com>
2018-11-06 22:44:38 -0300

    - New database methods for new feature

--- Transmitter/src/managers/DatabaseManager.js
+++ Transmitter/src/managers/DatabaseManager.js
@@ -18,10 +18,10 @@ class DatabaseManager {
     }
   };
 
-  addNewCard = newCard => {
+  addNewCard = async newCard => {
     const newTransaction = new CardSchema(newCard);
 
-    newTransaction.save(error => {
+    await newTransaction.save(error => {
       if (error) {
         throw new Error('Error in the database');
       }
@@ -38,6 +38,29 @@ class DatabaseManager {
       throw new Error('Card not emitted by Transmitter');
     }
     return response;
+  };
+
+  addCardTransactions = async transaction => {
+    const newTransaction = {
+      amount: transaction.amount,
+      date: transaction.date,
+      status: 'OK',
+    };
+    CardSchema.transactions.push(newTransaction);
+    await CardSchema.save();
+  };
+
+  updateCardTransactions = async (transactionId, status) => {
+    const transaction = await CardSchema.transactions.id(transactionId);
+    transaction.status = status;
+    await CardSchema.save();
+  };
+
+  deleteCardTransactions = async transactionId => {
+    const amount = await CardSchema.transactions.id(transactionId).amount;
+    const removed = await CardSchema.transactions.id(transactionId).remove();
+    console.log(removed);
+    return amount;
   };
 
   updateCardBalance = async (transactionId, amount) => {
