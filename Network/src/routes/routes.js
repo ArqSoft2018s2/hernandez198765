bc239a13f9ca1bdfcdf320990a86c52f252d9eba
Nicolas Hernandez <nicolas@xmartlabs.com>
2018-11-17 13:58:27 -0300

    - Refactor, add redisManager

--- Network/src/routes/routes.js
+++ Network/src/routes/routes.js
@@ -1,13 +1,10 @@
 import NetworkController from '../networking/controllers/NetworkController';
 
-const appRouter = (app, redisClient) => {
+const appRouter = app => {
   app.post('/Network', async (req, res) => {
     try {
       const transaction = req.body;
-      const response = await NetworkController.fraudControl(
-        transaction,
-        redisClient,
-      );
+      const response = await NetworkController.fraudControl(transaction);
       res.status(200).send({ ...response, aprobada: 'aprobada' });
     } catch (error) {
       res.status(500).send(error.message);
