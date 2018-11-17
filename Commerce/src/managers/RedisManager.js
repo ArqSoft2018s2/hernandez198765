db08a8a59a34675823e5b46e6ede482ed1f84a73
Nicolas Hernandez <nicolas@xmartlabs.com>
2018-11-17 14:04:32 -0300

    - Changes

--- Commerce/src/managers/RedisManager.js
+++ Commerce/src/managers/RedisManager.js
@@ -1,5 +1,7 @@
 import redis from 'redis';
 
 const redisClient = redis.createClient();
-
+redisClient.on('error', error => {
+  console.log('Something went wrong ', error);
+});
 export default redisClient;
--- PayYouNow/src/managers/RedisManager.js
+++ PayYouNow/src/managers/RedisManager.js
@@ -1,5 +1,8 @@
 import redis from 'redis';
 
 const redisClient = redis.createClient();
+redisClient.on('error', error => {
+  console.log('Something went wrong ', error);
+});
 
 export default redisClient;
